587d1379a70786f2718b41d589458bdc212a1891
derekt5-1620677 <derekt5@uw.edu>
2018-03-05 20:48:13 -0800

    Library maps & ggplot for server

--- server.R
+++ server.R
@@ -1,11 +1,21 @@
 # place your server code here
 library(ggplot2)
+library(dplyr)
+library(maps)
 
-# Data
-# <- read.csv()
+##### Data Wrangling #####
+## Here we get the data we want ##
+available.locations.df <- read.csv("available_locations.csv",
+                                   stringsAsFactors = FALSE)
+ncol.of.first.avail.locations.df <- ncol(available.locations.df)
+available.locations.df <- 
+  available.locations.df[, 2:ncol.of.first.avail.locations.df]
 
+us.locations.df <- filter(available.locations.df, country == "United States")
+
+## Now we output a map of all the trends for each city.
 server <- function(input, output) {
   output$map <- renderPlot({
-    # ggplot()
+    ggplot()
   })
 }
