4b208c9ac2c0ff6fdbaf06242f694e1b5712c377
derekt5-1620677 <derekt5@uw.edu>
2018-03-05 18:25:16 -0800

    Add commented code for map and prepare ggplot

--- server.R
+++ server.R
@@ -1,1 +1,11 @@
 # place your server code here
+library(ggplot2)
+
+# Data
+# <- read.csv()
+
+server <- function(input, output) {
+  output$map <- renderPlot({
+    # ggplot()
+  })
+}
--- ui.R
+++ ui.R
@@ -1,1 +1,22 @@
 # place your ui code here
+ui <- fluidPage(
+  # Title
+  titlePanel("Tweet Words 201"),
+  
+  # Main Panel
+  mainPanel(
+    # Heading
+    h2("Map")
+    
+    # Tabs
+    # tabsetPanel(
+    #   type = "tabs", 
+    #   tabPanel(
+    #     "Map"
+    #   )
+    # )
+    
+  )
+  
+  
+)
