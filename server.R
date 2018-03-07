67a191e69ebb1eed5a1866026c2e90180845c087
derekt5-1620677 <derekt5@uw.edu>
2018-03-06 22:35:18 -0800

    Add space in the server function

--- server.R
+++ server.R
@@ -5,18 +5,21 @@ library(dplyr)
 library(maps)
 
 ##### Data Wrangling #####
-## Here we get the data we want ##
+## Here we filter for US locations only ##
 available.locations.df <- read.csv("available_locations.csv",
                                    stringsAsFactors = FALSE)
 ncol.of.first.avail.locations.df <- ncol(available.locations.df)
+# The following line gets rid of the row numbers on the data frame
 available.locations.df <- 
   available.locations.df[, 2:ncol.of.first.avail.locations.df]
-
+# Filtering for US locations
 us.locations.df <- filter(available.locations.df, country == "United States")
 
-
-## Now we output a map of all the trends for each city.
+## We want to get trends for the US as a whole, and then compare these to ##
+## the trends of each available city. ##
 server <- function(input, output) {
+  
+  
   
   output$map <- renderPlot({
     
