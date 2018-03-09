9590dd5619284131da6484a121b749e119c6b4b8
Florrie Li <florrie@D-10-18-217-137.dhcp4.washington.edu>
2018-03-08 16:58:28 -0800

    final

--- flcode.R
+++ flcode.R
@@ -4,12 +4,18 @@ library('dplyr')
 # creates the UI for the app, with a widget on the side and a tab containing data table
 fl.ui <- fluidPage(
   sidebarPanel(
-    textInput("city.name", label = "Enter a Country")
+    textInput("city.name", label = "Enter a Country Name"),
+    helpText("lower case (i.e. canada)")
   ),
   mainPanel(
     tabsetPanel(type = "tabs",
-                tabPanel("Location Summary", textOutput("fl.countrytext"), tableOutput("fl.countrytable"))
-               )
+                tabPanel("Location Summary", textOutput("fl.countrytext"), tableOutput("fl.countrytable")),
+                          h2("Interpretation"), helpText("This table shows the worldwide locations of trending tweets.
+                                 The data is dependent on the number of tweets in a certain
+                                                         city. We want this information because it tells us where news
+                                                         travels to and what type of people care (i.e. people in the 
+                                                         city vs people in the suburbs)."))
+                
   )
 )
 
@@ -23,6 +29,7 @@ fl.server <- function(input, output) {
   fl.trends.count <- reactive({
     fl.trends.namecount <- fl.trends.data %>%
       filter(name != "Worldwide") %>%
+      mutate(country = tolower(country)) %>%
       filter(country == input$city.name) %>%
       summarize(total = n())
     return(fl.trends.namecount)
@@ -46,12 +53,19 @@ fl.server <- function(input, output) {
   fl.trends.country <- reactive({
     fl.trends.name <- fl.trends.data %>%
       filter(name != "Worldwide") %>%
+      mutate(country = tolower(country)) %>%
       select(country, name) %>%
       filter(country == input$city.name)
     return(fl.trends.name)
   })
   # outputting the table
   output$fl.countrytable <- renderTable(fl.trends.country())
+  
+  output$fl.mytext <- renderText("This table shows the worldwide locations of trending tweets.
+                                 The data is dependent on the number of tweets in a certain
+                                 city. We want this information because it tells us where news
+                                 travels to and what type of people care (i.e. people in the 
+                                 city vs people in the suburbs).")
 }
 
 shinyApp(ui = fl.ui, server = fl.server)
